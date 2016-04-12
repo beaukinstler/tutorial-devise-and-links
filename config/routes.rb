54bab9b36b596aa51d24b051e51f74c8b641cec5
Beau Kinstler <git@beaukinstler.info>
2016-04-12 10:56:43 -0500

    removing the devise users and bookmark scaffol

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'site/index'
 
-  devise_for :users
-  resources :bookmarks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
